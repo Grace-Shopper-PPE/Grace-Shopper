f6f7731e7f9e9d0a6b05fde3e0cb3bebe0d73d72
Ariel Wainer <arielwainer4@gmail.com>
2020-08-05 22:09:59 -0400

    created single product cards

--- client/components/single-product-details.js
+++ client/components/single-product-details.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 /**
  * COMPONENT
  */
 const SingleProductDetail = props => {
   const {name, price, imageUrl} = props.product
+  const newPrice = (price / 100).toFixed(2)
   return (
-    <div>
-      <p>{name}</p>
-      <p>{price}</p>
-      <img src={imageUrl} alt={name} />
+    <div className="m-3">
+      <Card style={{width: '18rem'}}>
+        <Card.Img variant="top" src={imageUrl} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>${newPrice}</Card.Text>
+          <Button variant="primary">Add To Cart</Button>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
