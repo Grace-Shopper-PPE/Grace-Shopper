5910822c91caa3373590cd7ac90441b44272cfd6
Sara Culhane <sculs@Saras-MacBook-Pro.local>
2020-08-06 17:07:57 -0400

    feat: updated nav-bar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,22 +3,55 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {Nav, NavItem, NavLink} from 'react-bootstrap'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>Grace Shopper PPE </h1>
     <nav>
       {isLoggedIn ? (
         <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
+          {/* <Link to="/home">Home</Link>
           <Link to="/products">All Products</Link>
           <Link to="/products/masks">Masks</Link>
           <Link to="/products/faceshields">Face Shields</Link>
-          <Link to="/products/sanitizers">Sanitizers</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
+          <Link to="/products/sanitizers">Sanitizers</Link> */}
+
+          <Nav variant="tabs" activeKey="/home">
+            <NavItem>
+              <NavLink href="/home">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink eventKey="link-1" href="/products">
+                All Products
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink eventKey="link-2" href="/products/masks">
+                Masks
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink eventKey="link-3" href="/products/faceshields">
+                Face Shields
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink eventKey="link-4" href="/products/sanitizers">
+                Sanitizers
+              </NavLink>
+            </NavItem>
+            <Nav className="justify-content-end">
+              <NavItem>
+                <NavLink href="/home">Profile</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#" onClick={handleClick}>
+                  Logout
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Nav>
         </div>
       ) : (
         <div>
