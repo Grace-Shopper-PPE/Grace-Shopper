5681903b5ec3bf0fbd2f7ef7006305e6b993f282
Lu Wang <loowang@hotmail.com>
2020-08-07 14:53:30 -0400

    fix: fixed minor bug

--- client/components/product-update.js
+++ client/components/product-update.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ProductForm from './product-form'
 
-export class ProductUpdate extends React.Component {
+export default class ProductUpdate extends React.Component {
   constructor() {
     super()
     this.state = {
