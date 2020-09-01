cffe3ecb3030d69179505039b06911b31a3c7334
Lu Wang <loowang@hotmail.com>
2020-09-01 02:40:30 -0400

    chore: clean up app

--- client/app.js
+++ client/app.js
@@ -1,26 +1,12 @@
 import React from 'react'
-import {Navbar} from './components'
 import Routes from './routes'
-// import { me } from './store'
-// import { connect } from 'react-redux'
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        {/* <Navbar /> */}
         <Routes />
       </div>
     )
   }
 }
-
-// const mapState = ({ currentUser }) => ({ currentUser })
-
-// const mapDispatch = dispatch => ({
-//   loadInitialData: () => {
-//     dispatch(me())
-//   }
-// })
-
-// export default connect(mapState, mapDispatch)(App)
